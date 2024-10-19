09d63d46917cc0660dcfcc70e6ff24ccf48ceede
Faisal-zia <engr.faisalzia@gmail.com>
2024-10-19 18:42:59 +0000

    new-project.py

---
+++ OOPS.py
@@ -0,0 +1,17 @@
+#creating class
+class Student:
+    name = "Mohid zia"
+    
+   # creating object(instance) 
+s1 = Student()
+print(s1.name)
+
+# creating factory making cars
+
+class Car:
+    color = "Green"
+    brand = "mercedes"
+    
+car1 = Car()
+print(car1.color)
+print(car1.brand)
---
+++ project.py
@@ -0,0 +1,17 @@
+class University:
+    def __init__(self, name):
+        self.name = name
+        self.departments = []
+university = University()
+
+#  class Person:
+#    def __init__(self, name, age, contact, role):
+#        self.name = name
+#        self.age = age
+#           self.contact = contact
+#          self.role = role
+#  p1 = Person("naveed", 28, 23234234, "teacher")
+#  p2 = Person("abu huraira", 21, 4545765, "teacher")
+# p3 = Person("faisal zia", 30, 34342, "student")
+#  p4 = Person("ahmad waqas",30,897568989, "student")
+#  print(p3.name, p3.contact)
